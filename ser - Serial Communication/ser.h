be453a285c68fc6a5f5645dfe856f3d83c32907c
Michael Graves <iamnewhb@gmail.com>
2018-08-04 18:28:53 -0700

    First Commit, added scheduler and serial libraries

---
+++ ser - Serial Communication/ser.h
@@ -0,0 +1,27 @@
+/*
+ * ser.h
+ *
+ *  Created on: Jul 26, 2018
+ *      Author: Michael Graves
+ */
+
+#ifndef SER_H_
+#define SER_H_
+
+//#############################################//
+//                Serial Functions
+//#############################################//
+void SerialWrite(char character);
+void SerialPrint(char string[]);
+void SerialPrintln(char string[]);
+void SerialPrintInt(int integer);
+void SerialPrintlnInt(int integer);
+
+char* SerialEndOfString(char string[]);
+uint8_t SerialLengthOfString(char string[]);
+int SerialStringToInt(char string[]);
+
+uint8_t InitSerial(void (*serialOnCharReceived)(char), void (*serialOnLineReceived)(volatile char*, volatile char*));
+void SerialInterruptHandler(void);
+
+#endif /* SER_H_ */
